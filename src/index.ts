e7c6d15aae51ac4a4dbd35217fd00404bfe5b995
Lukas Trombach <TrombachL@gmail.com>
2023-08-15 22:35:31 +1200

    automate download buttons

--- src/index.ts
+++ src/index.ts
@@ -22,24 +22,41 @@ puppeteer
     await page.setRequestInterception(true);
     page.on('request', interceptedRequest => {
         if (interceptedRequest.url().includes("-apidata.googleusercontent.com/download/storage")) {
-            console.info(`Intercepted ${interceptedRequest.url()}`);
+          console.log(interceptedRequest.url());
             urls.write(`${interceptedRequest.url()}\n`);
         }
-        
+
         interceptedRequest.continue();
     });
 
+    // wait for "Show exports" button
+    const exportButton = await page.waitForSelector("div[jsaction='click:npT2md']", { timeout: 180_000 })
+    if (!exportButton) {
+      throw new Error("Could not find export button");
+    }
+    const buttonText = await (await (await exportButton.$("span[jsname=V67aGc]"))?.getProperty('textContent'))?.jsonValue();
+    if (!buttonText || buttonText !== "Show exports") {
+      throw new Error("Export button does not have expected text");
+    }
 
-    const element = await page.waitForSelector("div.I7OXgf.nxteG.ZEeHrd.Inn9w.iWO5td", {visible: true, timeout: 180_000});
-    console.log(element);
+    exportButton.click();
 
-    // query download buttons
-    const buttons = await page.$$("td:not(.yrYG6) a[aria-label=Download]");
+    // wait for dialog
+    await page.waitForSelector("div.I7OXgf.nxteG.ZEeHrd.Inn9w.iWO5td", { visible: true, timeout: 180_000 });
 
-    for (const button of buttons) {
+        // query download buttons
+    const nButtons = (await page.$$("td:not(.yrYG6) a[aria-label=Download]")).length;
+
+    if (nButtons === 0) {
+      throw new Error("No download buttons found");
+    }
+
+    // click download buttons
+    for (let i = 0; i < nButtons; i++) {
+      const button = (await page.$$("td:not(.yrYG6) a[aria-label=Download]"))[i];
         button.click();
         await page.waitForRequest(request => request.url().includes("-apidata.googleusercontent.com/download/storage"));
-    };
+    }
 
     await browser.close();
   })
